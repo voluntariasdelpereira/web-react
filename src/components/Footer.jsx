6163daae504211d3b8d1e789c45d8531feef8287
Juan Pablo Garbora <juan@attuneiot.com>
2024-04-29 15:25:49 -0500

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,8 @@ export function Footer () {
                      display: 'flex',
                      flexDirection: 'column',
                      gap: '40px',
-                     backgroundColor: 'rgba(0, 0, 0, 0.05)'
+                     backgroundColor: 'rgba(0, 0, 0, 0.05)',
+                     bottom: 0
                  }}
             >
                 voluntariasdelpereira.org.uy ® 2006 - All rights reserved.
